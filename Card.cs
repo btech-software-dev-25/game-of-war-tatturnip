ed473cba0721c41fe1737e7336464eb2ae04c51f
tatturnip <deepsight02@gmail.com>
2026-01-15 14:20:48 -0700

    Completed Game of War code

--- Card.cs
+++ Card.cs
@@ -2,19 +2,21 @@ namespace GameOfWar
 {
     public class Card
     {
-
-        
+        // Create a string property Suit with a private setter
+        // Create an int property Rank with a private setter
         public string Suit { get; private set; }
 
         public int Rank { get; private set; }
 
+        // Create a public constructor that takes suit and rank as arguments and assigns them to Suit and Rank
         public Card(string suit, int rank)
         {
             Suit = suit;
             Rank = rank;
         }
 
-        
+        // Overload the > operator to compare two cards by rank
+        // Overload the < operator to compare two cards by rank
         public static bool operator >(Card card1, Card card2)
         {
             return card1.Rank > card2.Rank;
@@ -24,7 +26,7 @@ namespace GameOfWar
         {
             return card1.Rank < card2.Rank;
         }
-
+        // Create a public string method RankString that returns a string representation of this card's rank, 2-10 and Jack, Queen, King, Ace
 
         public string RankString()
         {
--- Program.cs
+++ Program.cs
@@ -1,48 +1,110 @@
 ﻿using GameOfWar;
 
-// Create an instance of the GameState class
-// Shuffle CardDeck within your instance
-// Deal 26 cards each from CardDeck to your instance's PlayerDeck and ComputerDeck
-
-
-// Create a function with the signature: static bool PlayCards(GameState state, int playerCardIndex)
-// The function should:
-//     Pull the card at playerCardIndex from state.PlayerDeck
-//     Pull the card at index 0 from state.ComputerDeck
-//     Compare the two cards
-//         If the player card is higher, the player gets both cards along with any in state.TableDeck
-//         If the computer card is higher, the computer gets both cards along with any in state.TableDeck
-//         If the player and computer cards are the same, both cards go into state.TableDeck
-//     Check whether either state.PlayerDeck or state.ComputerDeck are empty
-//         If the computer deck is empty, the player wins and state.Winner should be set to "Computer"
-//         If the player deck is empty, the computer wins and state.Winner should be set to "Player"
-//     return true
-
-
-// Call Lib.RunGame(), passing two parameters: the state object you instantiated above and the name of your PlayCards function
 
 namespace GameOfWar
 {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the GameState class
+            GameState state = new GameState();
+
+            // Shuffle CardDeck within your instance
+            state.CardDeck.Shuffle();
+
+            // Deal 26 cards each from CardDeck to your instance's PlayerDeck and ComputerDeck
+            state.PlayerDeck.PushCards(state.CardDeck.Deal(26));
+            state.ComputerDeck.PushCards(state.CardDeck.Deal(26));
+
+            // Call Lib.RunGame(), passing two parameters: the state object you instantiated above and the name of your PlayCards function
+            Lib.RunGame(state, PlayCards);
+        }
+
+
+        // Create a function with the signature: static bool PlayCards(GameState state, int playerCardIndex)
+        static bool PlayCards(GameState state, int playerCardIndex)
+        {
+            //     Pull the card at playerCardIndex from state.PlayerDeck
+            Card playerCard = state.PlayerDeck.PullCardAtIndex(playerCardIndex);
+            //     Pull the card at index 0 from state.ComputerDeck
+            Card computerCard = state.ComputerDeck.PullCardAtIndex(0);
+
+
+            //     Compare the two cards
+            if (playerCard > computerCard)
+            {
+                //         If the player card is higher, the player gets both cards along with any in state.TableDeck
+                state.PlayerDeck.PushCard(playerCard);
+                state.PlayerDeck.PushCard(computerCard);
+                state.PlayerDeck.PushCards(state.TableDeck.PullAllCards());
+            }
+            else if (playerCard < computerCard)
+            {
+                //         If the computer card is higher, the computer gets both cards along with any in state.TableDeck
+                state.ComputerDeck.PushCard(playerCard);
+                state.ComputerDeck.PushCard(computerCard);
+                state.ComputerDeck.PushCards(state.TableDeck.PullAllCards());
+            }
+            else
+            {
+
+                //         If the player and computer cards are the same, both cards go into state.TableDeck
+
+                state.TableDeck.PushCard(playerCard);
+                state.TableDeck.PushCard(computerCard);
+            }
+            //     Check whether either state.PlayerDeck or state.ComputerDeck are empty
+            if (state.PlayerDeck.Count == 0)
+            {
+                //         If the computer deck is empty, the player wins and state.Winner should be set to "Computer"
+                state.Winner = "Computer";
+                return true;
+            }
+
+            if (state.ComputerDeck.Count == 0)
+            {
+                //         If the player deck is empty, the computer wins and state.Winner should be set to "Player"
+                state.Winner = "Player";
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+
+
     public class GameState
     {
         // Create a public Deck property called CardDeck
-
+        public Deck CardDeck { get; set; }
 
         // Create a public Deck property called PlayerDeck
-
+        public Deck PlayerDeck { get; set; }
 
         // Create a public Deck property called ComputerDeck
-
+        public Deck ComputerDeck { get; set; }
 
         // Create a public Deck property called TableDeck
-
+        public Deck TableDeck { get; set; }
 
         // Create a public string property called Winner
-
+        public string Winner { get; set; }
 
         // Create a public constructor that accepts no parameters. It should:
         //    Initialize Winner to be empty (not null)
         //    Initialize CardDeck to be a new, fresh deck of 52 cards
         //    Initialize PlayerDeck, ComputerDeck, and TableDeck to be empty Deck objects (no cards)
+
+        public GameState()
+        {
+            Winner = "";
+            CardDeck = new Deck(new List<Card>(), false);
+            PlayerDeck = new Deck(new List<Card>(), true);
+            ComputerDeck = new Deck(new List<Card>(), true);
+            TableDeck = new Deck(new List<Card>(), true);
+        }
     }
 }
